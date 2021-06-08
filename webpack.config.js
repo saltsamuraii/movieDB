e548dff8c90b36034020fb641075d79b67076075
Kirill <piksarus@gmail.com>
2021-06-08 18:34:56 +0300

    add: webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exports: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Movies DB",
+            template: path.resolve(__dirname, "public/index.html"),
+            filename: "index.html",
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        compress: true,
+        port: 8000,
+        historyApiFallback: true,
+        hot: true,
+    },
+}
