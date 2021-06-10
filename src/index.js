eb9d054e13be25318f65fb79ee941cff30fd0832
Kirill <piksarus@gmail.com>
2021-06-10 17:23:50 +0300

    add commands for dev and prod mod

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
     render() {
         return (
             <div>
-
+                <h1>Hello</h1>
             </div>
         );
     };
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
