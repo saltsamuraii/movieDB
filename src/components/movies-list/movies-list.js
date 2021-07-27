f3691e77e45f801a7e9bfaa4277a82614a624081
Kirill <piksarus@gmail.com>
2021-07-28 02:43:22 +0300

    moviesList functional component

--- src/components/movies-list/movies-list.js
+++ src/components/movies-list/movies-list.js
@@ -1,36 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 import MovieCard from '../movie-card/movie-card';
 
-class MoviesList extends Component {
+function MoviesList(props) {
+    const {loading, movies, onErrorImage, onMovieSelected} = props
 
+    if (loading) return <h1>Loading...</h1>
+    if (!movies) return <h2>No movies found</h2>
 
-    render() {
-        const {loading, movies, onErrorImage, onMovieSelected} = this.props
-
-        if (loading) return <h1>Loading...</h1>
-        if (!movies) return <h2>No movies found</h2>
-
-        return (
-            <ul className="movies">
-                {movies.map(({id, poster_path, title, release_date, genres}) => (
-                        <li className="movie-card"
-                            key={id}
-                            data-id={id}>
-                            <MovieCard
-                                id={id}
-                                onMovieSelected={onMovieSelected}
-                                onErrorImage={onErrorImage}
-                                poster={poster_path}
-                                title={title}
-                                release={release_date}
-                                genre={genres}
-                            />
-                        </li>
-                    )
-                )}
-            </ul>
-        );
-    }
+    return (
+        <ul className="movies">
+            {movies.map(({id, poster_path, title, release_date, genres}) => (
+                    <li className="movie-card"
+                        key={id}
+                        data-id={id}>
+                        <MovieCard
+                            id={id}
+                            onMovieSelected={onMovieSelected}
+                            onErrorImage={onErrorImage}
+                            poster={poster_path}
+                            title={title}
+                            release={release_date}
+                            genre={genres}
+                        />
+                    </li>
+                )
+            )}
+        </ul>
+    );
 }
 
 export default MoviesList;
