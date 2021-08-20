81da65a042f7d1afbe844114d009a1064a4f12d4
Kirill <piksarus@gmail.com>
2021-08-20 23:24:51 +0300

    add resource test

--- src/helpers/resourse.test.tsx
+++ src/helpers/resourse.test.tsx
@@ -1,16 +1,42 @@
-import fetchMock from 'jest-fetch-mock';
+import { enableFetchMocks, ErrorOrFunction } from 'jest-fetch-mock';
 import { loadData } from './resourсe';
 
-fetchMock.enableMocks();
+
+enableFetchMocks();
+
+fetchMock.mockResponseOnce(JSON.stringify({foo: "bar"}), {
+  status: 200,
+  headers: [
+    ["Content-Type", "application/json"]
+  ]
+});
 
 describe('loadData module', () => {
-  it('', () => {
-    fetchMock.mockResponseOnce(JSON.stringify({ object: { url: 'https://reactjs-cdp.herokuapp.com/movies' } }));
-    //const object = loadData('https://reactjs-cdp.herokuapp.com/movies', undefined);
-    const requestUrl = `https://reactjs-cdp.herokuapp.com/movies`;
-
-    //expect(fetch).toHaveBeenCalledWith(requestUrl);
-    expect(fetch).toHaveBeenCalled();
-    //expect(fetch).toEqual(requestUrl);
+  it('should call right url', () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ data: { url: 'url' } }));
+    loadData('https://reactjs-cdp.herokuapp.com/movies', undefined);
+    expect(fetchMock).toBeCalledWith('https://reactjs-cdp.herokuapp.com/movies');
   });
+  it('url and params not to be undefined', () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ url: 'url' }));
+    loadData('https://reactjs-cdp.herokuapp.com/movies', {
+      search: 'string',
+      sortOrder: 'string',
+      searchBy: 'string',
+      sortBy: 'string'
+    });
+    expect(fetchMock).toBeCalledWith(
+      'https://reactjs-cdp.herokuapp.com/movies?search=string&sortOrder=string&searchBy=string&sortBy=string'
+    );
+  });
+  it('should call api', () => {
+    fetchMock.mockResponseOnce(JSON.stringify({ url: 'url' }));
+    loadData('https://reactjs-cdp.herokuapp.com/movies');
+    expect(fetchMock).toHaveBeenCalled()
+  })
+  /*it('should show error on wrong url',() => {
+    const errorMessage = 'oops'
+    fetchMock.mockRejectOnce(new Error(errorMessage));
+      expect(loadData('www.google.com')).rejects.toEqual('www.com')
+  });*/
 });
