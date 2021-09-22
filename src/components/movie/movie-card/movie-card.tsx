8d3ff0d8b8461c8a812c02488225af79158197a9
Kirill <piksarus@gmail.com>
2021-09-22 14:54:10 +0300

    rewrite movie-card on react-hooks

--- src/components/movie/movie-card/movie-card.tsx
+++ src/components/movie/movie-card/movie-card.tsx
@@ -1,68 +1,43 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import './movie-card.css';
 import { Movie } from '../movie';
 
-interface MovieCardState {
-  imageError: boolean
-}
-
 interface MovieCardProps {
-  data: Movie
-  onMovieSelected: (id: number) => void,
+  data: Movie;
+  onMovieSelected: (id: number) => void;
 }
 
-export default class MovieCard extends PureComponent<MovieCardProps, MovieCardState> {
-  constructor(props: MovieCardProps) {
-    super(props);
+export default function MovieCard({ data, onMovieSelected }: MovieCardProps) {
+  const [imageError, setImageError] = useState(true);
 
-    this.state = {
-      imageError: true
-    };
-
-    this.handleSelected = this.handleSelected.bind(this);
-    this.handleErrorImage = this.handleErrorImage.bind(this);
-  }
-
-  handleSelected(): void {
-    const { onMovieSelected, data: { id } } = this.props;
+  const handleSelected = (): void => {
+    const { id } = data;
     onMovieSelected(id);
-  }
+  };
 
-  handleErrorImage(): void {
-    this.setState({
-      imageError: false
-    });
-  }
+  const handleErrorImage = (): void => {
+    setImageError(false);
+  };
 
-  render() {
-    const { imageError } = this.state;
-    const {
-      data: {
-        poster_path: poster,
-        release_date: releaseDate,
-        title,
-        genres
-      }
-    } = this.props;
+  const { poster_path: poster, release_date: releaseDate, title, genres } = data;
 
-    const imgSrc = !imageError ? 'https://allmovies.tube/assets/img/no-poster.png' : poster;
+  const imgSrc = !imageError ? 'https://allmovies.tube/assets/img/no-poster.png' : poster;
 
-    return (
-      <>
-        <img
-          className="movie-card__poster"
-          src={imgSrc}
-          onClick={this.handleSelected}
-          onError={this.handleErrorImage}
-          alt=""
-          role="presentation"
-        />
-        <div className="movie-card__content">
-          <h5 className="movie-card__title">{title}</h5>
-          <span className="movie-card__year">{releaseDate.slice(0, 4)}</span>
-        </div>
-        <p className="movie-card__genre">{genres[0]}</p>
-      </>
-    );
-  }
+  return (
+    <>
+      <img
+        className="movie-card__poster"
+        src={imgSrc}
+        onClick={handleSelected}
+        onError={handleErrorImage}
+        alt=""
+        role="presentation"
+      />
+      <div className="movie-card__content">
+        <h5 className="movie-card__title">{title}</h5>
+        <span className="movie-card__year">{releaseDate.slice(0, 4)}</span>
+      </div>
+      <p className="movie-card__genre">{genres[0]}</p>
+    </>
+  );
 }
