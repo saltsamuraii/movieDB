34698819df0282f1e009063225cf9a7c73eda8bc
Kirill <piksarus@gmail.com>
2021-09-22 17:48:37 +0300

    rewrite movie-details on hooks, added eslint require-default-props to off

--- src/components/movie/movie-details/movie-details.tsx
+++ src/components/movie/movie-details/movie-details.tsx
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Movie } from '../movie';
 import './movie-details.css';
-
-interface MovieDetailsState {
-  imageError: boolean;
-}
 
 interface MovieDetailsProps {
   movie?: Movie;
@@ -14,85 +10,67 @@ interface MovieDetailsProps {
   resetMovie: () => void;
 }
 
-export default class MovieDetails extends Component<MovieDetailsProps, MovieDetailsState> {
-  constructor(props: MovieDetailsProps) {
-    super(props);
+export default function MovieDetails({
+  movie,
+  movieId,
+  onBack,
+  onLoadMovie,
+  resetMovie,
+}: MovieDetailsProps) {
+  const [imageError, setImageError] = useState(true);
 
-    this.state = {
-      imageError: true,
+  useEffect((): void => {
+    const updateMovie = () => {
+      if (movieId === undefined) {
+        resetMovie();
+        return;
+      }
+      onLoadMovie(`https://reactjs-cdp.herokuapp.com/movies/${movieId}`);
     };
 
-    this.handleErrorImage = this.handleErrorImage.bind(this);
+    updateMovie();
+  }, [movieId]);
+
+  const handleErrorImage = (): void => {
+    setImageError(false);
+  };
+
+  if (!movie) {
+    return null;
   }
 
-  componentDidMount(): void {
-    this.updateMovie();
-  }
+  const {
+    poster_path: poster,
+    vote_average: voteAverage,
+    release_date: releaseDate,
+    title,
+    genres,
+    runtime,
+    overview,
+  } = movie;
 
-  componentDidUpdate(prevProps: MovieDetailsProps): void {
-    const { movieId } = this.props;
-    if (movieId !== prevProps.movieId) {
-      this.updateMovie();
-    }
-  }
+  const imgSrc = !imageError ? 'https://allmovies.tube/assets/img/no-poster.png' : poster;
 
-  handleErrorImage(): void {
-    this.setState({
-      imageError: false,
-    });
-  }
-
-  updateMovie() {
-    const { movieId, resetMovie, onLoadMovie } = this.props;
-
-    if (movieId === undefined) {
-      resetMovie();
-      return;
-    }
-    onLoadMovie(`https://reactjs-cdp.herokuapp.com/movies/${movieId}`);
-  }
-
-  render() {
-    const { imageError } = this.state;
-    const { onBack, movie } = this.props;
-
-    if (!movie) {
-      return null;
-    }
-
-    const {
-      poster_path: poster,
-      vote_average: voteAverage,
-      release_date: releaseDate,
-      title,
-      genres,
-      runtime,
-      overview,
-    } = movie;
-
-    const imgSrc = !imageError ? 'https://allmovies.tube/assets/img/no-poster.png' : poster;
-
-    return (
-      <div className="movie-details__container">
-        <img
-          className="movie-details__poster"
-          src={imgSrc}
-          onError={this.handleErrorImage}
-          alt=""
-          role="presentation"
-        />
-        <div className="movie-details__content">
-          <span className="movie-details__title">{title}</span>
-          <span className="movie-details__rating">{voteAverage}</span>
-          <p className="movie-details__genre">{genres[0]}</p>
-          <span className="movie-details__year">{releaseDate.slice(0, 4)}</span>
-          <span className="movie-details__duration">{runtime} min</span>
-          <p className="movie-details__description">{overview}</p>
-          <button type="button" className="movie-details__button" onClick={onBack}>
-            Return
-          </button>
-        </div>
+  return (
+    <div className="movie-details__container">
+      <img
+        className="movie-details__poster"
+        src={imgSrc}
+        onError={handleErrorImage}
+        alt=""
+        role="presentation"
+      />
+      <div className="movie-details__content">
+        <span className="movie-details__title">{title}</span>
+        <span className="movie-details__rating">{voteAverage}</span>
+        <p className="movie-details__genre">{genres[0]}</p>
+        <span className="movie-details__year">{releaseDate.slice(0, 4)}</span>
+        <span className="movie-details__duration">{runtime} min</span>
+        <p className="movie-details__description">{overview}</p>
+        <button type="button" className="movie-details__button" onClick={onBack}>
+          Return
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
