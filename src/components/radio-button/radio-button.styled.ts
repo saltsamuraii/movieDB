555392bc30313a26484e4a9e668b54791ca5aa50
Kirill <piksarus@gmail.com>
2021-10-06 16:45:43 +0300

    jest styled components

--- src/components/radio-button/radio-button.styled.ts
+++ src/components/radio-button/radio-button.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const RadioButtonInput = styled.input`
-  display: none;
+  display: block;
 `;
--- src/components/radio-button/radio-button.test.tsx
+++ src/components/radio-button/radio-button.test.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { RadioButton } from './index';
+import 'jest-styled-components';
 
 describe('RadioButton component', () => {
   it('should call onChange when button clicked', () => {
     const onChange = jest.fn();
     render(
       <fieldset>
-        <RadioButton
-          name='button'
-          value='1'
-          defaultChecked
-          className='button'
-          onChange={onChange}/>
-        <RadioButton
-          name='button'
-          value='2'
-          className='button'
-          onChange={onChange}/>
-      </fieldset>);
+        <RadioButton name="title" value="1" defaultChecked className="button" onChange={onChange} />
+        <RadioButton name="genre" value="2" className="button" onChange={onChange} />
+      </fieldset>
+    );
     userEvent.click(screen.getAllByRole('radio')[1]);
     expect(onChange).toHaveBeenCalled();
   });
