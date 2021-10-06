74cb0c19bd91d24866ff888d293f229990453e32
Kirill <piksarus@gmail.com>
2021-10-06 16:56:36 +0300

    when display flex tests passes

--- src/components/radio-button/radio-button.test.tsx
+++ src/components/radio-button/radio-button.test.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import { RadioButton } from './index';
-import 'jest-styled-components';
 
 describe('RadioButton component', () => {
   it('should call onChange when button clicked', () => {
     const onChange = jest.fn();
     render(
       <fieldset>
-        <RadioButton name="title" value="1" defaultChecked className="button" onChange={onChange} />
+        <RadioButton defaultChecked name="title" value="1" className="button" onChange={onChange} />
         <RadioButton name="genre" value="2" className="button" onChange={onChange} />
       </fieldset>
     );
