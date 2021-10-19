6c734b550613804a069b2f1262053add05ac2769
Kirill <piksarus@gmail.com>
2021-10-19 17:33:43 +0300

    fix document app and searchMovie page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { store } from '../redux/store';
 import { GlobalStyles } from '../components/app/app.styled';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Movie Finder</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <GlobalStyles />
       <Provider store={store}>
         <Component {...pageProps} />
--- src/pages/search/[searchMovie].tsx
+++ src/pages/search/[searchMovie].tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { AppProps } from 'next/app';
 import { store } from '../../redux/store';
-import { SearchBar } from '../../components/search-bar';
-import { SearchInfo } from '../../components/search-info';
-import { MovieList } from '../../components/movie/movie-list';
+import { App } from '../../components/app';
 
-export default function SearchMovie({ pageProps }: AppProps) {
+export default function SearchMovie() {
   return (
     <Provider store={store}>
-      <SearchBar {...pageProps} />
-      <SearchInfo {...pageProps} />
-      <MovieList />
+      <App />
     </Provider>
   );
 }
